d0b5be60cfe97f1fd67f983986c652b5ed472bbf
Ayan Raza <ayanraza197@gmail.com>
2023-03-30 21:58:12 +0530

    Menu

---
+++ Exercise_05/app.js
@@ -0,0 +1,7 @@
+ let Item_Select = (x) => {
+    let Breakfast=document.querySelector("Breakfast")
+    console.log(Breakfast)
+    Breakfast.style.display="none"
+   console.log(x);
+   alert("hello");
+ };
