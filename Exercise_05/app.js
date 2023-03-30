6b58999666f20652f3f3a36c1a324df0e4054ff4
Ayan Raza <ayanraza197@gmail.com>
2023-03-31 02:24:51 +0530

    Menu

--- Exercise_05/app.js
+++ Exercise_05/app.js
@@ -1,7 +1,35 @@
- let Item_Select = (x) => {
-    let Breakfast=document.querySelector("Breakfast")
-    console.log(Breakfast)
-    Breakfast.style.display="none"
-   console.log(x);
-   alert("hello");
- };
+"use strict";
+
+const Breakfast = document.querySelector(".Breakfast");
+const btnBreakfast = document.querySelector(".btnBreakfast");
+
+
+btnBreakfast.addEventListener("click", function () {
+Breakfast.classList.remove('Breakfast');
+console.log("click");
+});
+
+const Lunch = document.querySelector(".Lunch");
+const btnLunch = document.querySelector(".btnLunch");
+
+btnLunch.addEventListener("click", function () {
+  Lunch.classList.remove("Lunch");
+  console.log("click");
+});
+
+const Shakes = document.querySelector(".Shakes");
+const btnshakes = document.querySelector(".btnShakes");
+
+btnshakes.addEventListener("click", function () {
+  Shakes.classList.remove("Shakes");
+  console.log("click");
+});
+
+const Dinner = document.querySelector(".Dinner");
+const btnDinner = document.querySelector(".btnDinner");
+
+btnDinner.addEventListener("click", function () {
+  Dinner.classList.remove("Dinner");
+  console.log("click");
+});
+
