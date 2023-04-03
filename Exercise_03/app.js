14b64de1a37d75cc05cc1bee42e10d3e112b0ea6
Ayan Raza <ayanraza197@gmail.com>
2023-04-04 00:38:12 +0530

    Simple Calculator

--- Exercise_03/app.js
+++ Exercise_03/app.js
@@ -1,33 +1,4 @@
-
-// let Cal = (x) => {
-//   let nm1 = document.querySelector(".num1").value;
-//   let nm2 = document.querySelector(".num2").value;
-//   let res = document.querySelector(".res");
-//   let operator = x.value;
-
-//   nm1 = Number(nm1);
-//   nm2 = Number(nm2);
-
-//   switch (operator) {
-//     case "+":
-//       console.log('+');
-      
-//       res.innerHTML = nm1 + nm2;
-//       break;
-
-//     case "-":
-//       res.innerHTML = nm1 - nm2;
-//       break;
-
-//     case "*":
-//       res.innerHTML = nm1 * nm2;
-//       break;
-
-//     case "/":
-//       res.innerHTML = nm1 / nm2;
-//       break;
-//   }
-// };
+"use strict"
 // plus operation
  addNum =document.querySelector('.addNum')
  const plusOperation = function () {
